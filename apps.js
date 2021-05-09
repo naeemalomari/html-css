98e99574fdb422ce6c3c6252023f10b07bde2f22
naeemalomari <>
2021-05-09 11:51:04 +0000

    changes

--- apps.js
+++ apps.js
@@ -2,9 +2,11 @@ var UserName = prompt ('HELLO');
 var UserName1 = prompt ('whats your name')
 alert ( ' Welcome '  + UserName1 )
 var UserPlayer = prompt ("who is the best football player in the wolrd Messi or Ronaldo ?");
+
 if (UserPlayer === 'messi' ) {
   document.write (' <img src="https://th.bing.com/th/id/R03686cd0a3b7dd268ee30c16b78bf7fd?rik=XHMYWUrLchBZ8A&pid=ImgRaw">')
   }
+
   else if(UserPlayer === 'ronaldo')
    { 
     document.write (' GO OUT OF HERE '); 
@@ -12,11 +14,16 @@ if (UserPlayer === 'messi' ) {
     alert( ' Go back and choose one ' );
   }
    var userFavClub = prompt ( 'what is your favourite club ?' ) 
+   function knowBestPlayer(){
+
    while( userFavClub !== 'barchelona' && userFavClub !== 'barca' ) {  
     userFavClub = prompt ( "choose barca or barchelona please ! ")
      }
+     knowBestPlayer();
+   }
      var photos = prompt('How many photos do you want to see?')
-     var image = ''
+     
+     var image = ""
      for (var i=0;i<photos;i++) {
        if (userFavClub === 'barca' )  
        {
@@ -24,4 +31,4 @@ if (UserPlayer === 'messi' ) {
         } else if  (userFavClub === 'barchelona' ) {
           image = image + '<img scr="https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5f15af31465263000625ce08%2F0x0.jpg">'}
         }
-        document.write (image) 
+        document.write (image)
